806b64720409e94f001d4a166cbbda1f26cf27e9
German <gercger5@gmail.com>
2024-07-03 19:01:41 -0300

    Avances intl de instrumentelista

--- src/app/[locale]/instrumentista/page.tsx
+++ src/app/[locale]/instrumentista/page.tsx
@@ -3,7 +3,6 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-
 import { SwiperSlide, Swiper } from "swiper/react";
 import "swiper/css";
 import { BsYoutube, BsSpotify } from "react-icons/bs";
@@ -15,49 +14,53 @@ import {
 } from "@/components/ui/tooltip";
 import { Separator } from "@/components/ui/separator";
 import WorkSliderBtns from "@/components/WorkSliderBtns";
-import { instrumentalista } from "@/constants/instrumentalista";
+import instrumentalista from "@/constants/instrumentalista";
 
 
 const Instrumentista = () => {
-  const [proyecto, setProyecto] = useState(instrumentalista[0]);
+  const instrumentList = instrumentalista();
+  const [proyecto, setProyecto] = useState(instrumentList[0]);
 
-  const handleSlideChange = (swiper: { activeIndex: any }) => {
+  const handleSlideChange = (swiper: { activeIndex: number }) => {
     // get current slide index
     const currentIndex = swiper.activeIndex;
     // update proyecto state based on current slide index
-    setProyecto(instrumentalista[currentIndex]);
+    setProyecto(instrumentList[currentIndex]);
   };
 
+
+  console.log(proyecto);
+  
   return (
-    <div className=" min-h-[80vh] w-10/12 mx-auto flex flex-col justify-center py-12 xl:px-0">
+    <div className="min-h-[80vh] w-10/12 mx-auto flex flex-col justify-center py-12 xl:px-0">
       <div className="xl:mb-32">
-        <h1 className="tracking-widest text-amber-100 text-2xl  lg:text-4xl lg:mt-14  text-center ">
+        <h1 className="tracking-widest text-amber-100 text-2xl lg:text-4xl lg:mt-14 text-center">
           INSTRUMENTISTA
         </h1>
-        <Separator className=" mt-8 lg:w-1/2 w-5/6 bg-amber-50 mx-auto" />
+        <Separator className="mt-8 lg:w-1/2 w-5/6 bg-amber-50 mx-auto" />
       </div>
 
-      <div className="container -my-10  mx-auto ">
+      <div className="container -my-10 mx-auto">
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
           <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
             <div className="flex flex-col gap-[30px] h-[50%]">
               {/* nombre banda */}
-              <div className="text-xl leading-none text-amber-100 font-medium uppercase tracking-widest ">
-                {proyecto.banda}
+              <div className="text-xl leading-none text-amber-100 font-medium uppercase tracking-widest">
+                {proyecto.nombreAlbum}
               </div>
-              {/* nombre cancion */}
-              <div className="text-lg leading-none text-slate-50  font-medium tracking-wider ">
-                {proyecto.nombreDisco}
+              {/* nombre disco */}
+              <div className="text-lg leading-none text-slate-50 font-medium tracking-wider">
+                {proyecto.nombreAutor}
               </div>
 
               {/* proyecto descripcion */}
-              <p className="text-neutral-400">{proyecto.descripcion}</p>
+              <p className="text-neutral-400">{proyecto.texto}</p>
               {/* instrumentos */}
               <ul className="flex gap-4">
                 {proyecto.instrumentos.map((item, index) => {
                   return (
                     <li key={index} className="text-md tracking-wider text-amber-50">
-                      {item.name}
+                      {item.nombre}
                       {/* remove the last comma */}
                       {index !== proyecto.instrumentos.length - 1 && ","}
                     </li>
@@ -68,41 +71,41 @@ const Instrumentista = () => {
               <div className="border w-2/3 border-white/20"></div>
               {/* buttons */}
               <div className="flex items-center gap-4">
-                {/* discoCompleto proyecto button */}
-                <Link
-                  href={proyecto.discoCompleto}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <TooltipProvider delayDuration={100}>
-                    <Tooltip>
-                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-neutral-900 flex justify-center items-center group">
-                        <BsSpotify className="text-white text-2xl group-hover:text-amber-100" />
-                      </TooltipTrigger>
-                      <TooltipContent className="bg-neutral-100">
-                        <p>Ir a Spotify</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </Link>
+                {/* Boton de spotify */}
+                {proyecto.urlSpotify ? (
+                  <Link href={proyecto.urlSpotify} target="_blank" rel="noopener noreferrer">
+                    <TooltipProvider delayDuration={100}>
+                      <Tooltip>
+                        <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-neutral-900 flex justify-center items-center group">
+                          <BsSpotify className="text-white text-2xl group-hover:text-amber-100" />
+                        </TooltipTrigger>
+                        <TooltipContent className="bg-neutral-100">
+                          <p>Ir a Spotify</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </Link>
+                ) : (
+                  <></>
+                )}
 
-                {/* youtube button */}
-                <Link
-                  href={proyecto.youtube}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <TooltipProvider delayDuration={100}>
-                    <Tooltip>
-                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-neutral-900 flex justify-center items-center group">
-                        <BsYoutube className="text-white text-2xl group-hover:text-amber-100" />
-                      </TooltipTrigger>
-                      <TooltipContent className="bg-neutral-100">
-                        <p>Ver video completo</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </Link>
+                {/* Boton de youTube */}
+                {proyecto.urlYoutube ? (
+                  <Link href={proyecto.urlYoutube} target="_blank" rel="noopener noreferrer">
+                    <TooltipProvider delayDuration={100}>
+                      <Tooltip>
+                        <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-neutral-900 flex justify-center items-center group">
+                          <BsYoutube className="text-white text-2xl group-hover:text-amber-100" />
+                        </TooltipTrigger>
+                        <TooltipContent className="bg-neutral-100">
+                          <p>Ver video completo</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </Link>
+                ) : (
+                  <></>
+                )}
               </div>
             </div>
           </div>
@@ -113,14 +116,13 @@ const Instrumentista = () => {
               className="xl:h-[520px]"
               onSlideChange={handleSlideChange}
             >
-              {instrumentalista.map((proyecto, index) => {
+              {instrumentList.map((proyecto, index) => {
                 return (
                   <SwiperSlide key={index} className="w-full">
-                    <div className="h-[460px] relative group flex justify-center items-center ">
-                      {/* image */}
+                    <div className="h-[460px] relative group flex justify-center items-center">
                       <div className="relative w-full h-full">
                         <Image
-                          src={proyecto.imagen}
+                          src={proyecto.imagen.src}
                           fill
                           className="object-scale-down"
                           alt="Fotos de Agustín como instrumentista"
@@ -132,8 +134,8 @@ const Instrumentista = () => {
               })}
               {/* slider buttons */}
               <WorkSliderBtns
-                containerStyles="flex gap-2 absolute left-0 bottom-[calc(50%_-_22px)]  z-20 w-full justify-between "
-                btnStyles="bg-default hover:bg-amber-100 border-solid border border-neutral-600  text-slate-100 hover:text-neutral-600 text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+                containerStyles="flex gap-2 absolute left-0 bottom-[calc(50%_-_22px)] z-20 w-full justify-between"
+                btnStyles="bg-default hover:bg-amber-100 border-solid border border-neutral-600 text-slate-100 hover:text-neutral-600 text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
                 iconsStyles={undefined}
               />
             </Swiper>
@@ -141,6 +143,7 @@ const Instrumentista = () => {
         </div>
       </div>
     </div>
+
   );
 };
 
--- src/constants/instrumentalista.tsx
+++ src/constants/instrumentalista.tsx
@@ -1,59 +1,101 @@
-import laImbailable from "@/imagenes/laImbailable.jpeg";
-import luTrentiniPepe from "@/imagenes/luciaTrentiniPepe.jpg";
-import laNonna from "@/imagenes/laNonna.jpg";
-import { StaticImageData } from "next/image";
+import { useTranslations } from "next-intl";
 
 interface Instrumento {
-  name: string;
+  nombre: string;
 }
 
-interface Banda {
-  banda: string;
-  nombreDisco: string;
-  descripcion: string;
+interface InstrumentalistaType {
+  nombreAlbum: string;
+  nombreAutor: string;
+  fecha: string;
   instrumentos: Instrumento[];
-  imagen: StaticImageData;
-  discoCompleto: string;
-  youtube: string;
+  texto: string;
+  urlSpotify: string;
+  urlYoutube: string;
+  imagen: {
+    src: string;
+    width: number;
+    height: number;
+    alt: string;
+  };
 }
 
-export const instrumentalista: Banda[] = [
-  {
-    banda: "La Imbailable Cumbia Orquesta",
-    nombreDisco: "La Quinqueña",
-    descripcion:
-      "Toque con la imbailable en el 2020. Tocan cumbia colombiana y esta muy guay!",
-    instrumentos: [{ name: "Guitarra" }],
-    imagen: laImbailable,
-    discoCompleto:
-      "https://open.spotify.com/intl-es/album/7lJrgRP80KcBMpG2MWFoir?si=AclovCIhSsKn_T9MXSsRdw",
-    youtube: "https://www.youtube.com/watch?v=zMK7GiA2fEE",
-  },
-  {
-    banda: "Lucía Trentini",
-    nombreDisco: "Ya no se",
-    descripcion:
-      "Filmando en “Espacio cultural Alberto Soriano” ECAS, Montevideo.",
-    instrumentos: [
-      { name: "Guitarra" },
-      { name: "Flauta" },
-      { name: "Clarinete" },
-      { name: "Bandoneón" },
-    ],
-    imagen: luTrentiniPepe,
-    discoCompleto:
-      "https://open.spotify.com/intl-es/artist/58oUUw92iA5bwaNnVo5irI?si=OhNR9R_rT7W-Ipd6LoZK1A",
-    youtube: "https://www.youtube.com/watch?v=0peEL7LJAtY",
-  },
-  {
-    banda: "La Nonna",
-    nombreDisco: "La Nonna",
-    descripcion: "La nonna es una gran banda",
-    instrumentos: [{ name: "Guitarra" }],
-    imagen: laNonna,
-    discoCompleto:
-      "https://open.spotify.com/intl-es/artist/5SjKMiIDZ18plsdulpFvpb?si=h_Z5I2hPQmm3S3tYioz6ow",
-    youtube:
-      "https://www.youtube.com/watch?v=f6QiLbbMdRU&list=PLRWFGPmySWDAKKan7eWl-lMdtmH-hghpk",
-  },
-];
+const Instrumentalista = (): InstrumentalistaType[] => {
+  const t = useTranslations("Instrumentalista");
+
+  const instrumentalistaArray = t.raw("instrumentalistaItems") as {
+    nombreAlbum: string;
+    nombreAutor: string;
+    fecha: string;
+    instrumentos: Instrumento[];
+    texto: string;
+    urlSpotify: string;
+    urlYoutube: string;
+    imagen: {
+      src: string;
+      width: number;
+      height: number;
+      alt: string;
+    };
+  }[];
+
+  const instrumentalistaItems: InstrumentalistaType[] = instrumentalistaArray.map(
+    (item) => ({
+      nombreAlbum: item.nombreAlbum,
+      nombreAutor: item.nombreAutor,
+      fecha: item.fecha,
+      instrumentos: item.instrumentos,
+      texto: item.texto,
+      urlSpotify: item.urlSpotify,
+      urlYoutube: item.urlYoutube,
+      imagen: {
+        src: item.imagen.src,
+        width: item.imagen.width,
+        height: item.imagen.height,
+        alt: item.imagen.alt
+      }
+    })
+  );
+
+  return instrumentalistaItems;
+};
+
+export default Instrumentalista;
+// {
+//   banda: "La Imbailable Cumbia Orquesta",
+//   nombreDisco: "La Quinqueña",
+//   descripcion:
+//     "Toque con la imbailable en el 2020. Tocan cumbia colombiana y esta muy guay!",
+//   instrumentos: [{ name: "Guitarra" }],
+//   imagen: laImbailable,
+//   discoCompleto:
+//     "https://open.spotify.com/intl-es/album/7lJrgRP80KcBMpG2MWFoir?si=AclovCIhSsKn_T9MXSsRdw",
+//   youtube: "https://www.youtube.com/watch?v=zMK7GiA2fEE",
+// },
+// {
+//   banda: "Lucía Trentini",
+//   nombreDisco: "Ya no se",
+//   descripcion:
+//     "Filmando en “Espacio cultural Alberto Soriano” ECAS, Montevideo.",
+//   instrumentos: [
+//     { name: "Guitarra" },
+//     { name: "Flauta" },
+//     { name: "Clarinete" },
+//     { name: "Bandoneón" },
+//   ],
+//   imagen: luTrentiniPepe,
+//   discoCompleto:
+//     "https://open.spotify.com/intl-es/artist/58oUUw92iA5bwaNnVo5irI?si=OhNR9R_rT7W-Ipd6LoZK1A",
+//   youtube: "https://www.youtube.com/watch?v=0peEL7LJAtY",
+// },
+// {
+//   banda: "La Nonna",
+//   nombreDisco: "La Nonna",
+//   descripcion: "La nonna es una gran banda",
+//   instrumentos: [{ name: "Guitarra" }],
+//   imagen: laNonna,
+//   discoCompleto:
+//     "https://open.spotify.com/intl-es/artist/5SjKMiIDZ18plsdulpFvpb?si=h_Z5I2hPQmm3S3tYioz6ow",
+//   youtube:
+//     "https://www.youtube.com/watch?v=f6QiLbbMdRU&list=PLRWFGPmySWDAKKan7eWl-lMdtmH-hghpk",
+// },
